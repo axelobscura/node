cde4bc36de9f959342961b506bc2fcc7e801a0d9
Axel Obscura <axosar@gmail.com>
2020-05-28 09:19:55 -0500

    Routing requets

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    //process.exit();
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>ENTER MESSAGE</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SUBMIT</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>HOla</title></head>');
-    res.write('<body><h1>HOLA</h1></body>');
-    res.write('</html>');
-    res.end();
+
 });
 
 server.listen(3000);
