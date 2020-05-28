1defdd827048823cb934c4952851047d8b8c4d4f
Axel Obscura <axosar@gmail.com>
2020-05-28 09:57:16 -0500

    Event driven execution code

--- app.js
+++ app.js
@@ -17,18 +17,21 @@ const server = http.createServer((req, res) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parseBody = Buffer.concat(body).toString();
             const message = parseBody.split('=')[1];
             fs.writeFileSync('message.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
         });
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
-
+    res.write('<html>');
+    res.write('<head><title>ENTER MESSAGE</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SUBMIT</button></form></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
