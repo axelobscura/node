535becd89d209d5ed57e1637c3fec42bacb54f31
Axel Obscura <axosar@gmail.com>
2020-05-29 10:25:20 -0500

    Export module

--- app.js
+++ app.js
@@ -1,37 +1,9 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
 const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>ENTER MESSAGE</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SUBMIT</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
-        });
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>ENTER MESSAGE</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SUBMIT</button></form></body>');
-    res.write('</html>');
-    res.end();
+
+
 });
 
 server.listen(3000);
