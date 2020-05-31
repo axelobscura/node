42dd5b7c590a92ebfa83cd7111344bf3ff5ac5e6
Axel Obscura <axosar@gmail.com>
2020-05-31 05:22:53 -0500

    Quitar console

--- routes.js
+++ routes.js
@@ -13,7 +13,6 @@ const requestHandler = (req, res) => {
     if (url === '/message' && method === 'POST') {
         const body = [];
         req.on('data', (chunk) => {
-            console.log(chunk);
             body.push(chunk);
         });
         return req.on('end', () => {
