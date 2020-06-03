428bdbd792ad49e92b124d43f054dccdd0d77d4e
Axel Obscura <axosar@gmail.com>
2020-06-03 13:16:31 -0500

    Travelling to Middlewares with next

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const app = express();
 
 app.use((req, res, next) => {
     console.log("In the middleware!!!");
-    next();
+    next();  // Allows the request to continue to the next middleware
 });
 
 app.use((req, res, next) => {
     console.log("In the next middleware!!!");
+    res.send('<h1>Hello from express...</h1>');
 });
 
 const server = http.createServer(app);
