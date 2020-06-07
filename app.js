f76fd8c4ff24d0738bbc0d8d33e80a942d81775e
Axel Obscura <axosar@gmail.com>
2020-06-06 19:48:28 -0500

    Parsing incoming requests

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded());
+
 app.use('/add-product', (req, res, next) => {
-    console.log("In the next middleware!!!");
-    res.send('<h1>The add product page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">ENVIAR</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
 });
 
 app.use('/', (req, res, next) => {
-    console.log("In the next middleware!!!");
     res.send('<h1>Hello from express...</h1>');
 });
 
