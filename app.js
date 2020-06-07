d60d790496f80df1bec7ea447876430a8e0c115c
Axel Obscura <axosar@gmail.com>
2020-06-06 19:59:41 -0500

    Middleware for post

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">ENVIAR</button></form>');
 });
 
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect("/");
 });
