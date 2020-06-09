54754c43591c591df4b3603a116a963bfe704d01
Axel Obscura <axosar@gmail.com>
2020-06-09 08:22:22 -0500

    Serving files statically PUBLIC FOLDER

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 
