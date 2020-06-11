80311316bc875e04fe643e910fcdd60a4e770251
Axel Obscura <axosar@gmail.com>
2020-06-11 08:22:49 -0500

    Sharing data across Requests and users

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const express = require('express');
 const rootDir = require('../util/path');
-const router = express.Router();
 const adminData = require('./admin');
+const router = express.Router();
+
 
 router.get('/', (req, res, next) => {
     console.log(adminData.products);
